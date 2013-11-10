496f80eb08ffe4eea298dc9bd993525a3e3d5d20
Alex Bahouth <abahouth@onekingslane.com>
2013-11-10 14:58:10 -0800

    Added examples of the tests from yodaetl for type enforcement- still need to break into unit tests.

--- jawsome-core/src/jawsome_core/xform/xforms/value_type_filter.clj
+++ jawsome-core/src/jawsome_core/xform/xforms/value_type_filter.clj
@@ -26,3 +26,61 @@
         (project-map path=>type :value-xform type=>satisfies-type?)]
     (fn [m]
       (reduce remove-offending-fields m path=>satisfies-type?))))
+
+
+;; {"okl_params": "{ \"sales_event_id\":\"12345a\", \"sku_id\":\"-9741*sfxd*139c91de-bd62-0601-d311-790a1dcb1fd5-0cb*sfxd*disableWs\" }"}
+;; {"okl_params": "{ \"sales_event_id\":\"12345\", \"sku_id\":\"-9741*sfxd*139c91de-bd62-0601-d311-790a1dcb1fd5-0cb*sfxd*disableWsa\" }"}
+;; {"okl_params": "{ \"sales_event_id\":\"12345\", \"sku_id\":\"9741\" }"}
+;; {"bool_test_prop": true}
+;; {"bool_test_prop": false}
+;; {"bool_test_prop": 17 }
+;; {"bool_test_prop": "17" }
+;; {"bool_test_prop": "this is a string" }
+
+;; # There are three acceptable types: number, string, and boolean
+;; type-enforcement:
+;;   - path: okl_params, sku_id
+;;     type: number
+;;   - path: okl_params, sales_event_id
+;;     type: number
+;;   - path: bool_test_prop
+;;     type: boolean
+
+;; (deftest does-type-enforcement-work-test
+;;   (testing "Generalization of type enforcement! woot!"
+;;     (let [[bad-sku-bad-sales-id
+;;            bad-sku-good-sales-id
+;;            good-sku-good-sales-id
+;;            good-bool-true
+;;            good-bool-false
+;;            bad-bool-num
+;;            bad-bool-num-as-str
+;;            bad-bool-str]
+;;           (clojure.string/split
+;;            (slurp "test/resources/transform/type-enforcement-sample-data.json")
+;;            #"\n")]
+;;       (testing "bad sku, bad sales id"
+;;         (is (= (transform bad-sku-bad-sales-id etl-config)
+;;                (list {}))))
+;;       (testing "bad sku, good sales id"
+;;         (is (= (transform bad-sku-good-sales-id etl-config)
+;;                (list {"sales_event_id" 12345}))))
+;;       (testing "good sku, good sales id"
+;;         (is (= (transform good-sku-good-sales-id etl-config)
+;;                (list {"sales_event_id" 12345,
+;;                       "sku_id", 9741}))))
+;;       (testing "good bool, true"
+;;         (is (= (transform good-bool-true etl-config)
+;;                (list {"bool_test_prop" true}))))
+;;       (testing "good bool, false"
+;;         (is (= (transform good-bool-false etl-config)
+;;                (list {"bool_test_prop" false}))))
+;;       (testing "bad bool, number"
+;;         (is (= (transform bad-bool-num etl-config)
+;;                (list {}))))
+;;       (testing "bad bool, number as string"
+;;         (is (= (transform bad-bool-num-as-str etl-config)
+;;                (list {}))))
+;;       (testing "bad bool, string"
+;;         (is (= (transform bad-bool-str etl-config)
+;;                (list {})))))))
