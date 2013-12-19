0a810ff2f73910251a67ebe981d340532e7c660e
Alex Bahouth <abahouth@onekingslane.com>
2013-12-19 12:41:11 -0800

    CLI initial commit.

---
+++ jawsome-cli/project.clj
@@ -0,0 +1,10 @@
+(defproject com.onekingslane.danger/jawsome-cli "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [com.onekingslane.danger/jawsome-core "0.1.0"]]
+  :profiles {:dev {:dependencies [[org.slf4j/slf4j-log4j12 "1.7.5"]]
+                   :resource-paths ["resources"]}})
---
+++ jawsome-cli/test/jawsome_cli/core_test.clj
@@ -0,0 +1,7 @@
+(ns jawsome-cli.core-test
+  (:use clojure.test
+        jawsome-cli.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
