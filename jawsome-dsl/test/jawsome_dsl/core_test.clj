a41fdecb1e80da3eac3133d52bd1c4bc6936c8d6
Matt Halverson <mhalverson@onekingslane.com>
2014-02-11 19:31:09 -0800

    Futzing around with library structure

---
+++ jawsome-dsl/test/jawsome_dsl/core_test.clj
@@ -0,0 +1,7 @@
+(ns jawsome-dsl.core-test
+  (:require [clojure.test :refer :all]
+            [jawsome-dsl.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
---
+++ jawsome-storm/src/jawsome_storm/core.clj
@@ -0,0 +1,6 @@
+(ns jawsome-storm.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
