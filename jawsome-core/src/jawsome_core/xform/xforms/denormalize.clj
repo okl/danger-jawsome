14bb8c1f4dd29a46909a1fb1f10c9925996c8162
Matt Halverson <mhalverson@onekingslane.com>
2014-02-14 17:52:13 -0800

    denormalize is now a direct alias for denormalize-map!
    Updated denormalize-map (in danger-denormal repo) so that it doesn't
    convert string-keys into keyword-keys. Now we don't have to namify
    every key of every map it gives back. So it's just
      (def denormalize denormalize-map).
    
    Leaving it aliased in case we ever need to change the definition.

--- jawsome-core/src/jawsome_core/xform/xforms/denormalize.clj
+++ jawsome-core/src/jawsome_core/xform/xforms/denormalize.clj
@@ -3,18 +3,7 @@
   {:author "Matt Halverson"
    :date "2/14/2014"}
   (:require [roxxi.utils.print :refer [print-expr]])
-  (:require [roxxi.utils.collections :refer [project-map]])
   (:require [denormal.core :refer [denormalize-map]]))
 
-(defn- namify [maybe-keyword]
-  "denormalize-map converts some of the keys into keywords.
-This function is useful for converting them back."
-  (if (keyword? maybe-keyword)
-    (name maybe-keyword)
-    maybe-keyword))
-
-(defn denormalize [json-map & optional-args]
-  (let [args (conj optional-args json-map)
-        denormalized-jsons (apply denormalize-map args)]
-    (map #(project-map % :key-xform namify)
-         denormalized-jsons)))
+;; It's just that easy.
+(def denormalize denormalize-map)
