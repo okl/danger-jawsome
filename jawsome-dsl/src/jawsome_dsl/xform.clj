60d434a006160f386e4fd1991f41cf299fb321f1
Alex Bahouth <abahouth@onekingslane.com>
2014-02-20 17:09:18 -0800

    * You can now bind variables and refer to them in your program. * You can supply thunks in case the data isn't available at initialization time * We've adjusted seqify to allow us to use xforms/xform for implementation   of the read-phase.

--- jawsome-dsl/src/jawsome_dsl/xform.clj
+++ jawsome-dsl/src/jawsome_dsl/xform.clj
@@ -6,6 +6,9 @@
             [diesel.core :refer [definterpreter]]
             [roxxi.utils.print :refer [print-expr]]
             [roxxi.utils.common :refer [def-]])
+  (:require [jawsome-core.reader.json.xforms.unicode :refer [unicode-recode]]
+            [jawsome-core.reader.json.xforms.cruft :refer [remove-cruft]]
+            [jawsome-core.reader.json.core :refer [make-json-reader]])
   (:use jawsome-core.xform.xforms.denormalize
         jawsome-core.xform.xforms.hoist
         jawsome-core.xform.xforms.property-mapping
@@ -15,42 +18,64 @@
         jawsome-core.xform.xforms.synonyms
         jawsome-core.xform.xforms.value-type-filter))
 
-(def- registry (atom {}))
+(def- sym-tab (atom {}))
 
 
-(defn register-xform! [k v]
-  (when (contains? @registry k)
+(defn defxform [k v]
+  (when (contains? @sym-tab k)
     (log/warnf "Attemping to overwrite already defined function %s" k))
-  (swap! registry #(assoc % k v)))
+  (swap! sym-tab #(assoc % k v)))
+
+(def defvar defxform)
 
 (defn xform-registry []
-  @registry)
+  @sym-tab)
 
-(register-xform! 'prune-nils (constantly prune-nils))
-(register-xform! 'reify (constantly reify-values))
-(register-xform! 'remap-properties make-property-remapper)
-(register-xform! 'value-type-filter make-value-type-filter)
-(register-xform! 'translate make-value-synonymizer)
-(register-xform! 'translate-paths make-path-specific-synonymizer)
-(register-xform! 'static-values static-value-merge-fn)
-(register-xform! 'default-values default-value-merge-fn)
+;; Read phase
+(defxform 'remove-cruft (constantly remove-cruft))
+(defxform 'unicode-recode (constantly unicode-recode))
+(defxform 'read-json make-json-reader)
+
+;; Xform phase
+;;hoist
+(defxform 'remap-properties make-property-remapper)
+(defxform 'reify (constantly reify-values))
+(defxform 'translate make-value-synonymizer)
+(defxform 'translate-paths make-path-specific-synonymizer)
+(defxform 'value-type-filter make-value-type-filter)
+(defxform 'prune-nils (constantly prune-nils))
+
+
+
+
+;; not ordered
+(defxform 'static-values static-value-merge-fn)
+(defxform 'default-values default-value-merge-fn)
 
 ;; We aren't using a more generic version here
 ;; either because we don't think it can exist
 ;; or we understand that the contract is we want
 ;; to generate functions that take a map
 ;; and return 0 or more maps- but always maps.
-(defn- seqify [m-or-s]
-  (if (map? m-or-s)
-    (list m-or-s)
-    (seq m-or-s)))
+(defn- seqify [map-str-or-seq]
+  (if (or (map? map-str-or-seq)
+          (string? map-str-or-seq))
+    (list map-str-or-seq)
+    (seq map-str-or-seq)))
 
 ;; (xform (lookup 'fn-id) & args-to-init-fn) => (fn [m] ...)
 (definterpreter xform-phase-interp [reg]
   ['xforms => :xforms]
   ['xform => :xform]
-  ['lookup => :lookup])
+  ['lookup => :lookup]
+  ['dethunk => :dethunk])
 
+
+
+
+(defmethod xform-phase-interp :dethunk [[_ thunk-val-expr] reg]
+  (let [thunk (xform-phase-interp thunk-val-expr reg)]
+    (if (fn? thunk) (thunk) thunk)))
 
 (defn- xforms-applier
   "Takes a collection of functions s.t. each function takes a map => map*
@@ -67,9 +92,12 @@ map* is short hand for a sequence of 0 or more maps
         (recur (mapcat (first xforms) results)
                (rest xforms))))))
 
+;; (xforms & body)
+;; (xforms desc & body) (descriptions are cool)
 (defmethod xform-phase-interp :xforms [[_ & exprs] reg]
   "Returns a function of map | map* => map*"
-  (let [inited-xforms (map #(xform-phase-interp % reg) exprs)
+  (let [exprs (if (string? (first exprs)) (rest exprs) exprs)
+        inited-xforms (map #(xform-phase-interp % reg) exprs)
         xforms-fn (xforms-applier inited-xforms)]
     (fn [m-or-ms]
       (xforms-fn (seqify m-or-ms)))))
@@ -78,7 +106,8 @@ map* is short hand for a sequence of 0 or more maps
 (defmethod xform-phase-interp :xform [expr reg]
   "Returns a function of map => map* "
   (let [[_ lookup-expr & init-args] expr
-        init-fn (xform-phase-interp lookup-expr reg)]
+        init-fn (xform-phase-interp lookup-expr reg)
+        init-args (map #(xform-phase-interp % reg) init-args)]
     (if (= init-fn :missing-fn)
       (let [msg (format "Unable to instantiate xform: %s" expr)]
         (log/errorf msg)
@@ -87,7 +116,7 @@ map* is short hand for a sequence of 0 or more maps
         (comp seqify inited-fn)))))
 
 (defmethod xform-phase-interp :lookup [[_ fn-id] reg]
-  (let [init-fn (get reg fn-id)]
+  (let [init-fn (print-expr (get reg fn-id))]
     (if (nil? init-fn)
       (do
         (log/errorf "Unknown function %s specified. Available: %s"
@@ -95,12 +124,15 @@ map* is short hand for a sequence of 0 or more maps
         :missing-fn)
       init-fn)))
 
+(defvar 'translate-cfg {"yes" true "no" false})
+(defvar 'remap-d-cfg (fn [] {:d "renamed!"}))
+
 (def the-program '(xforms
                     (xforms
-                     (xform (lookup translate) {"yes" true "no" false})
+                     (xform (lookup translate) (lookup translate-cfg))
                      (xform (lookup reify)))
                     (xforms
-                     (xform (lookup remap-properties) {:d "renamed!"})
+                     (xform (lookup remap-properties) (dethunk (lookup remap-d-cfg)))
                      (xform (lookup remap-properties) {:e "Also renamed!"})
                      (xform (lookup reify)))))
 
