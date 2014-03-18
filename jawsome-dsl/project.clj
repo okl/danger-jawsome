7592db427a67b4eb36f533169c20d3a59550c16f
Matt Halverson <mhalverson@onekingslane.com>
2014-03-18 15:46:47 -0700

    Keep track of cumulative observed schema during denorm phase :D

--- jawsome-dsl/project.clj
+++ jawsome-dsl/project.clj
@@ -7,7 +7,8 @@
                  [org.clojure/tools.logging "0.2.6"]
                  [com.onekingslane.danger/diesel "1.0.3"]
                  [com.onekingslane.danger/clojure-common-utils "0.0.23"]
-                 [com.onekingslane.danger/jawsome-core "0.3-SNAPSHOT"]]
+                 [com.onekingslane.danger/jawsome-core "0.3-SNAPSHOT"]
+                 [com.onekingslane.danger/jsonschema "1.0.1"]]
   :profiles {:dev {:dependencies [[org.slf4j/slf4j-log4j12 "1.7.5"]]
                    :resource-paths ["resources"]}}
   :main jawsome-dsl.core)
