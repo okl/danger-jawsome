5ae67fcacc26550e2e99b33fffd45a70925aa9cd
Matt Halverson <mhalverson@onekingslane.com>
2014-03-27 15:54:39 -0700

    Remove unused project

--- jawsome-pipeline/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject jawsome-pipeline "0.1.0-SNAPSHOT"
-  :description "Here lies the notion of ORDERING of transforms"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
