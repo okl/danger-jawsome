ca2965c6d86128519642415a7a712362ff21dab6
Matt Halverson <mhalverson@onekingslane.com>
2014-03-27 13:45:43 -0700

    First pass at a CLI wrapper w00000t!

--- jawsome-cli/project.clj
+++ jawsome-cli/project.clj
@@ -8,4 +8,5 @@
                  [com.onekingslane.danger/jawsome-core "0.3-SNAPSHOT"]
                  [com.onekingslane.danger/jawsome-dsl "0.3-SNAPSHOT"]]
   :profiles {:dev {:dependencies [[org.slf4j/slf4j-log4j12 "1.7.5"]]
-                   :resource-paths ["resources"]}})
+                   :resource-paths ["resources"]}}
+  :main jawsome-cli.core)
