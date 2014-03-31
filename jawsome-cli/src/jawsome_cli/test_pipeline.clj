813fc781f94e8116b0ecf62f8aaf2f95c6e72849
Matt Halverson <mhalverson@onekingslane.com>
2014-03-31 15:54:45 -0700

    Ugly io-handling, because Clojure macros make it hard to default to stdin/stdout.
    Also added a basic end-to-end test of jawsome.cli/core.
    
    In jawsome.cli/core, we have several commands where we want to
    (1) read from a file, defaulting to *in* if no input file is specified, or
    (2) write to a file, defaulting to *out* if no output file is specified.
    
    Using the `with-open` macro is not useful in this case, because it tries to
    close readers/writers you open with it. Closing *in* or *out* worked fine at
    the command line, but started to break down in the following situations:
    (1) writing unit tests
    (2) trying to write to the same writer twice (e.g. for `project` phase, when
        the header AND xsv-output are both going to *out*
    
    Threw in some hackish-feeling code to handle it all... I'm sure you understand.

--- jawsome-cli/src/jawsome_cli/test_pipeline.clj
+++ jawsome-cli/src/jawsome_cli/test_pipeline.clj
@@ -8,19 +8,6 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; TODO is this unit-test-worthy?
-
-;; (def raw (list "{\"a\": \"1\", \"b\": [\"2\", \"34\"]}" "{\"foo\": \"bazzle\"}" "{\"foo\": 123}"))
-;; (with-in-str (reduce str (interpose "\n" (map str raw)))
-;;   (-main "denorm")) ;; write to /tmp/denorm
-
-;; (def d (list {"a" "1", "b_arr" "2", "b_idx" 0} {"a" "1", "b_arr" "34", "b_idx" 1} {"foo" "bazzle"} {"foo" 123}))
-;; (with-in-str (reduce str (interpose "\n" (map str d)))
-;;   (-main "schema")) ;; write to /tmp/sch
-
-;; (-main "project" "/tmp/denorm" "/tmp/sch")
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; USAGE
 
 ;;lein uberjar; cat /tmp/raw | java -jar /Users/mhalverson/Code/okl/danger-jawsome/jawsome-cli/target/jawsome-cli-0.1.0-SNAPSHOT-standalone.jar denorm | tee /tmp/denorm | java -jar /Users/mhalverson/Code/okl/danger-jawsome/jawsome-cli/target/jawsome-cli-0.1.0-SNAPSHOT-standalone.jar schema > /tmp/schema ; java -jar /Users/mhalverson/Code/okl/danger-jawsome/jawsome-cli/target/jawsome-cli-0.1.0-SNAPSHOT-standalone.jar project --input /tmp/denorm --schema-path /tmp/schema --output /tmp/blat
@@ -30,50 +17,11 @@
 
 ;;CLEANUP
 
-;;X TODO first, hook into clojure.tools.cli library for cli args + options
-;;X TODO delimiter at command line for project phase
-;;X TODO the header should be delimited by delimiter, and get rid of ( and )
-;;X TODO no-headers option at command line for project phase (instead, you
-;;       specify the desired output path of the header file)
-;; TODOs from through rest of code
-;;    add unit tests from jawsome_dsl/core.clj
-;;    change the old jawsome-dsl.core/pipeline-interp to be denorm-interp,
-;;         update all the existing jawsome_dsl unit tests to use denorm-interp (?)
-;;    revisit separate_phases... read+xform are now denorm; project may not need to be considered
-;;X TODO allow specific file names rather than always reading from *in*, writing to *out*
-;;     (optional -- default is to read from *in* and write to *out*
-
-;; TODO (down the road) allow denorm+schema to happen together as a performance optimization
-
-
-
-;; the ... should be able to support arbitrary args supplied by the caller of the program, and we
-;; need to give them the ability to layer in their own preflight and postflight functions to be called
-;; around the invocations in main. This can be part of the def-cli etc...
-;; these functions might intialize DB stuff, loggers, etc.
-;;
-;; look at clojure-hadoop to see how this type of extensibility is generated through
-;; its configuration of defjob (https://github.com/alexott/clojure-hadoop/blob/master/src/clojure_hadoop/defjob.clj)
-;; if someone wants to combine xform + schema for the sake of performance, they should be able
-;; to do that by specifying it in the '(pipeline ...) and then passing in a command-line option
-;; to *1, where they specify the schema-output-path (schema.clj in the above)
-
-
-
-;; (def my-pipeline
-;;   '(pipeline
-;;     (xform-phase ...)))
-;; (def-cli-pipeline
-;;   :pipeline my-pipeline
-;;   :log-to "/an/absolute/path"
-;;   :db-cnxn "path/to/yml/file")
-
-
 ;;DEPLOY
 
 ;; figure out deploy strategy
 
-;; at this point, push and publish v1.0.0
+;; at this point, push and publish v1.0.0 for jawsome-core, jawsome-dsl, jawsome-cli
 
 ;;USE
 
--- jawsome-cli/test/jawsome_cli/core_test.clj
+++ jawsome-cli/test/jawsome_cli/core_test.clj
@@ -1,7 +1,55 @@
 (ns jawsome-cli.core-test
-  (:use clojure.test
-        jawsome-cli.core))
+  "Tests for jawsome-cli.core!"
+  {:author "Matt Halverson"
+   :date "2014/03/31"}
+  (:use clojure.test)
+  (:require [roxxi.utils.print :refer [print-expr]])
+  (:require [jawsome-cli.core :refer [def-cli-pipeline]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def simple-pipeline
+  '(pipeline
+    (denorm-phase
+     (read-phase (xforms :read-json))
+     (xform-phase (xforms :denorm)))
+    (schema-phase)
+    (project-phase)))
+
+(def-cli-pipeline simple-pipeline)
+
+(defn- string->buffered-reader [s]
+  (clojure.java.io/reader (java.io.StringReader. s)))
+
+(deftest end-to-end-test
+  (let [raw-records (list "{\"a\": \"1\", \"b\": [\"2\", \"34\"]}"
+                          "{\"foo\": \"bazzle\"}"
+                          "{\"foo\": 123}")
+        raw (reduce str (interpose "\n" (map str raw-records)))
+        d (with-out-str
+            (-main "denorm"
+                   "--input"
+                   (string->buffered-reader raw)))
+        s (with-out-str
+            (-main "schema"
+                   "--input"
+                   (string->buffered-reader d)))
+        delimiter "|"
+        p (with-out-str
+            (-main "project"
+                   "--input"
+                   (string->buffered-reader d)
+                   "--schema-path"
+                   (string->buffered-reader s)
+                   "--delimiter"
+                   delimiter))]
+    (testing "Denorm phase"
+      (is (= (map read-string (clojure.string/split d #"\n"))
+             (list {"a" "1", "b_arr" "2", "b_idx" 0}
+                   {"a" "1", "b_arr" "34", "b_idx" 1}
+                   {"foo" "bazzle"}
+                   {"foo" 123}))))
+    (testing "Schema phase"
+      (is (= (read-string s)
+             #jsonschema.type_system.types.Document{:properties #{"b_idx" "a" "b_arr" "foo"}, :map {"foo" #jsonschema.type_system.types.Union{:union-of #{#jsonschema.type_system.types.Str{:min 6, :max 6} #jsonschema.type_system.types.Int{:min 123, :max 123}}}, "a" #jsonschema.type_system.types.Str{:min 1, :max 1}, "b_arr" #jsonschema.type_system.types.Str{:min 1, :max 2}, "b_idx" #jsonschema.type_system.types.Int{:min 0, :max 1}}})))
+    (testing "Project phase"
+      (is (= (clojure.string/split p #"\n"))
+          ["a|b_arr|b_idx|foo" "1|2|0|" "1|34|1|" "|||bazzle" "|||123"]))))
