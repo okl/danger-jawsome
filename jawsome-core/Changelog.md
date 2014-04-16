f8b649658987cd684b5eb9085b20ccd8164ced8e
Matt Halverson <mhalverson@onekingslane.com>
2014-04-16 14:18:48 -0700

    Add a pass-through logging xform

---
+++ jawsome-core/Changelog.md
@@ -0,0 +1,7 @@
+# 1.1.0
+
+Added pass-through logging xform!
+
+# 1.0.0
+
+First stable version!
---
+++ jawsome-core/src/jawsome_core/xform/xforms/log.clj
@@ -0,0 +1,20 @@
+(ns jawsome-core.xform.xforms.log
+  "Implements xform pipeline step: log"
+  {:author "Matt Halverson"
+   :date "2014/04/15"}
+  (:require [roxxi.utils.print :refer [print-expr]])
+  (:require [clojure.tools.logging :as logger]))
+
+(defn log
+  ([m]
+     (log :info m))
+  ([level m]
+     (logger/log level m)
+     m))
+
+(defn make-log
+  ([]
+     (make-log :info))
+  ([level]
+     (fn [m]
+       (log level m))))
